98a373e3ffb6f83f72bec4a4fb777e4cf5bffd0d
shannone <shannone426@gmail.com>
2022-09-21 19:15:10 -0400

    added div elements in App

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Nav from "./Nav";
+import Filter from "./Filter";
+import HogList from "./HogList";
 
 import hogs from "../porkers_data";
 
 function App() {
+
+
 	return (
-		<div className="App">
-			<Nav />
+		<div className="ui grid container App">
+			<div className="sixteen wide column centered">
+			    <Nav />
+			</div>
+			<div className="sixteen wide column centered">
+				<Filter />
+			</div>
+			<div classNmae="sixteen wide column centered">
+				<HogList />
+			</div>
 		</div>
 	);
 }
