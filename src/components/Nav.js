e7be17e215510b85404392854deac2d0c5bc4387
shannone <shannone426@gmail.com>
2022-09-21 21:06:39 -0400

    added checkbox logic

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,11 +4,11 @@ import piggy from "../assets/porco.png";
 const Nav = () => {
   return (
     <div className="navWrapper">
-      <span className="headerText">Hogwarts</span>
+      <span className="headerText">HogWild!</span>
       <div className="TwirlyPig">
         <img src={piggy} className="App-logo" alt="piggy" />
       </div>
-      <span className="normalText">A React App for County Fair Hog Fans</span>
+      <span className="normalText">A React App for Extra Mini County Fair Hog Fans</span>
     </div>
   );
 };
