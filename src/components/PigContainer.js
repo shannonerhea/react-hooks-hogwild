8f43aa3fdfbce6025c87a9d4eefbb01e5cbec646
shannone <shannone426@gmail.com>
2022-09-25 19:49:49 -0400

    added sort functionality

--- src/components/PigContainer.js
+++ src/components/PigContainer.js
@@ -4,13 +4,29 @@ import FilteredHogs from "./FilteredHogs"
 
 
 function PigContainer({ hogs }) {
-
     const [checked, setChecked] = useState(false);
-    //console.log(checked)
-    //console.log(hogs)
     const [sortBy, setSortBy] = useState(false)
+
     const greasedFilteredHogs = hogs.filter((hog) => 
     checked ? checked === hog.greased : true)
+
+
+    
+
+    function sortHogs() {
+        if (sortBy === " name") {
+            const sortByName = [...greasedFilteredHogs].sort((hog1, hog2) => 
+    hog1.name.toLowerCase().localeCompare(hog2.name.toLowerCase())
+    );
+            return sortByName
+        } else if (sortBy === "weight" ) {
+            const sortByWeight = [...greasedFilteredHogs].sort((hog1, hog2) => hog1.weight - hog2.weight
+            );
+            return sortByWeight;
+
+        }
+    };
+ 
 
 
     const renderHogCards = greasedFilteredHogs.map((hog) => (
