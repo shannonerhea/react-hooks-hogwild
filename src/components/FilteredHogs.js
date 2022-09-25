ff8c24186d3a048c5d9b9195d272c6e49b372648
shannone <shannone426@gmail.com>
2022-09-25 17:47:26 -0400

    pig details and filter comp

--- src/components/FilteredHogs.js
+++ src/components/FilteredHogs.js
@@ -1,19 +1,25 @@
-
+import { useState } from "react";
 
 
 function FilteredHogs() { 
+    
+    const [checked, setChecked] = useState(false);
+
     function handleGreasedChange() {
-        console.log("changed")
+        //console.log("changed")
+        setChecked((checked) => !checked)
+
     };
 
-
-    return (
+     return (
         <div>
             <label htmlFor="greased">Greased</label>
             <input 
             type="checkbox"
             name="greased"
             id="greased"
+            onChange={handleGreasedChange}
+            checked={checked}
             />
         </div>
     )
